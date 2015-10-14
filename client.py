c84c4e2c62f085de135f19bb8089ea026def152b
DavidVictorG <davidvgiandly@gmail.com>
2015-10-14 22:25:45 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,98 +1,53 @@
-import sys
-import socket
-import select
-import pickle 
-import time
-import string
+# telnet program example
+import socket, select, string, sys
  
-def chat_client():
-
-	sys.stdout.write('Host : ')
-	host = sys.stdin.readline()
-	
-	sys.stdout.write('Port : ')
-	port = int(sys.stdin.readline())
+def prompt() :
+    sys.stdout.write('<You> ')
+    sys.stdout.flush()
+ 
+#main function
+if __name__ == "__main__":
      
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if(len(sys.argv) < 3) :
+        print 'Usage : python telnet.py hostname port'
+        sys.exit()
      
-	try :
-		s.connect((host, port))
-	except :
-		print 'Client is unable to connect'
-		sys.exit()
+    host = sys.argv[1]
+    port = int(sys.argv[2])
      
-	print 'Client is now connected to remote host. You can start sending messages'
-	sys.stdout.write('>> '); sys.stdout.flush()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
      
-	while 1:
-		socket_list = [sys.stdin, s]
-		 
-		# Get the list sockets which are readable
-		ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
-		 
-		for sock in ready_to_read:      
-		
-			if sock == s:
-				# incoming message from remote server, s
-				data = sock.recv(4096)
-				if not data :
-					print '\nYou are disconnected from chat server'
-					sys.exit()
-				else :
-					#print data
-					sys.stdout.write(data)
-					sys.stdout.write('>> '); sys.stdout.flush()     
-			
-			else :
-				# user entered a message
-				msg = []
-				#menyediakan input buat memasukkan kata
-				temp = sys.stdin.readline()
-				#temp1 buat command
-				temp1 = string.split(temp[:-1])
-				#d= panjang array temp1 tadi
-				d=len(temp1)
-				#command buat login
-				if temp1[0]=="login" :
-					#jika command login lebih dari 2 kata, maka invalid
-					if d>2:
-						print('Username Invalid')
-					#jika command login hanya nulis "login"
-					elif d<2:
-						print('Login need username')
-					else:
-						#kirim pesan ke socket server
-						s.send(temp)
-						
-				elif temp1[0]=="send" :
-					if d<3:
-						print('Invalid Send Command')
-					else:
-						s.send(temp)
-						
-				elif temp1[0]=="sendall" :
-					if d<2:
-						print("Invalid SendAll Command")
-					else:
-						s.send(temp)
-						
-				elif temp1[0]=="list" :
-					if d>1:
-						print('List doesnt have parameter')
-					else:
-						s.send(temp)
-						
-				elif temp1[0]=="whoami" :
-					if d>1:
-						print('Whoami doesnt have parameter')
-					else:
-						s.send(temp)
-						
-				else:
-					print ('Invalid Command')
-				
-				#s.send(temp)
-				sys.stdout.write('>> '); sys.stdout.flush() 
-
-
-chat_client()
+    # connect to remote host
+    try :
+        s.connect((host, port))
+    except :
+        print 'Unable to connect'
+        sys.exit()
+     
+    print 'Connected to remote host. Start sending messages'
+    prompt()
+     
+    while 1:
+        socket_list = [sys.stdin, s]
+         
+        # Get the list sockets which are readable
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+         
+        for sock in read_sockets:
+            #incoming message from remote server
+            if sock == s:
+                data = sock.recv(4096)
+                if not data :
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else :
+                    #print data
+                    sys.stdout.write(data)
+                    prompt()
+             
+            #user entered a message
+            else :
+                msg = sys.stdin.readline()
+                s.send(msg)
+                prompt()
