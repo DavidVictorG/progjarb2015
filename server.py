7b00bd9518bea50d3e89a1a36a091de7f803635c
DavidVictorG <davidvgiandly@gmail.com>
2015-10-14 22:24:58 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,212 +1,66 @@
-import sys
-import socket
-import select
-import pickle
-import string
-
-HOST = 'localhost' 
-SOCKET_LIST = []
-NAME_LIST = []
-RECV_BUFFER = 4096 
-PORT = 8888
-
-
-def chat_server():
-
-	sys.stdout.write('Port : ')
-	PORT = int(sys.stdin.readline())
-	
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-	server_socket.bind((HOST, PORT))
-	server_socket.listen(10)
-
-	SOCKET_LIST.append(server_socket)
-
-	print "The chat server is started on Port " + str(PORT)
-
-	while True:
-		ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
-	  
-		for sock in ready_to_read:
-			if sock == server_socket: 
-				sockfd, addr = server_socket.accept()
-				SOCKET_LIST.append(sockfd)
-				print "Client (%s, %s) is connected" % addr
-				 
-				broadcast(server_socket, sockfd, "[%s:%s] has joined the chat\n" % addr)
-			 
-			else:
-				# process data received from client, 
-				try:
-					
-					data = sock.recv(RECV_BUFFER)
-					#data = pickle.loads(data)
-					if data:
-						
-						#memisahkan komen dan pesan
-						
-						temp1 = string.split(data[:-1])
-						#d= panjang temp1
-						
-						d=len(temp1)
-						
-						#cek apakah login
-						if temp1[0]=="login" :
-							log_in(sock, str(temp1[1]))
-						#cek apakah send
-						elif temp1[0]=="send" :
-							#logged utk status sdg/blm
-							logged = 0
-							user = ""
-
-							for x in range (len(NAME_LIST)):
-								#penanda sudah login
-								if NAME_LIST[x]==sock:
-									logged=1
-									#input nama 
-									user=NAME_LIST[x+1]
-
-							if logged==0:
-								send_msg(sock, "Please login first\n")
-							else:
-								temp2=""
-								for x in range (len(temp1)):
-								 array temp1
-									if x>1:
-										if not temp2:
-											temp2+=str(temp1[x])
-										#jka temp2 sudah terisi,diisi "spasi"
-										else:
-											temp2+=" "
-											temp2+=str(temp1[x])
-								#send ke satu user
-								for x in range (len(NAME_LIST)):
-									if NAME_LIST[x]==temp1[1]:
-										send_msg(NAME_LIST[x-1], "["+user+"] : "+temp2+"\n")
-							'''
-							temp2=""
-							for x in range(len(temp1)):
-								if x!=0 and x!=1:
-									if not temp2:
-										temp2=str(temp1[x])
-									else:
-										temp2+=" "
-										temp2+=temp1[x]
-							send_msg(sock, str(temp1[1]),temp2+"\n")
-							'''
-								
-						elif temp1[0]=="sendall" :
-							
-							logged = 0
-							user = ""
-							for x in range (len(NAME_LIST)):
-								if NAME_LIST[x]==sock:
-									logged=1
-									user=NAME_LIST[x+1]
-							
-							if logged==0:
-								send_msg(sock, "Please login first\n")
-							
-							else:
-								temp2=""
-								for x in range(len(temp1)):
-									if x!=0:
-										if not temp2:
-											temp2=str(temp1[x])
-										else:
-											temp2+=" "
-											temp2+=temp1[x]
-								
-								broadcast(server_socket, sock, "["+user+"] : "+temp2+"\n")
-						
-						elif temp1[0]=="list" :
-							#send_msg(sock, "cobo\n")
-							logged = 0
-							for x in range (len(NAME_LIST)):
-								if NAME_LIST[x]==sock:
-									logged=1
-							
-							if logged==0:
-								send_msg(sock, "Please login first\n")
-							
-							else:
-								temp2=""
-								for x in range (len(NAME_LIST)):
-									
-									if x%2==1:
-										temp2+=" "
-										temp2+=str(NAME_LIST[x])
-								send_msg(sock, "[List_User] : "+temp2+"\n")
-							
-						elif temp1[0]=="whoami" :
-							g = 0
-							for name in range (len(NAME_LIST)):
-								if NAME_LIST[name]==sock:
-									g = 1
-									send_msg(sock, "Your username is "+str(NAME_LIST[name+1])+"\n")
-							if g==0:
-								send_msg(sock, "You haven't login\n")
-								
-						else:
-							print ('Invalid Command')
-					else:
-						# remove the socket that's broken    
-						if sock in SOCKET_LIST:
-							SOCKET_LIST.remove(sock)
-
-						# at this stage, no data means probably the connection has been broken
-						broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
-
-				# exception 
-				except:
-					broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
-					continue
-
-	server_socket.close()
-    
-# broadcast chat messages to all connected clients
-def broadcast (server_socket, sock, message):
-    for x in range (len(NAME_LIST)):
-		
-        # send the message only to peer
-        if NAME_LIST[x] != server_socket and NAME_LIST[x] != sock and x%2==0 :
-            try :
-                NAME_LIST[x].send(message)
-            except :
-                # broken socket connection
-                NAME_LIST[x].close()
-                # broken socket, remove it
-                if NAME_LIST[x] in SOCKET_LIST:
-                    SOCKET_LIST.remove(NAME_LIST[x])
+# Tcp Chat server
  
-def send_msg (sock, message):
-	try:
-		sock.send(message)
-	except:
-		sock.close()
-		
-		if sock in SOCKET_LIST:
-			SOCKET_LIST.remove(sock)
-
-def log_in (sock, user):
-	g = 0
-	f = 0
-	for name in NAME_LIST:
-		if name == user:
-			g = 1
-		if name == sock:
-			f = 1
-	
-	if f==1:
-		send_msg(sock, "You already has a username\n")
-	elif g==1:
-		send_msg(sock, "Username already exist\n")
-	else:
-		#masukkan data ke array
-		NAME_LIST.append(sock)
-		NAME_LIST.append(user)
-		send_msg(sock, "Login success\n")
-	
-chat_server()
+import socket, select
+ 
+#Function to broadcast chat messages to all connected clients
+def broadcast_data (sock, message):
+    #Do not send the message to master socket and the client who has send us the message
+    for socket in CONNECTION_LIST:
+        if socket != server_socket and socket != sock :
+            try :
+                socket.send(message)
+            except :
+                # broken socket connection may be, chat client pressed ctrl+c for example
+                socket.close()
+                CONNECTION_LIST.remove(socket)
+ 
+if __name__ == "__main__":
+     
+    # List to keep track of socket descriptors
+    CONNECTION_LIST = []
+    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
+    PORT = 5000
+     
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this has no effect, why ?
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(("0.0.0.0", PORT))
+    server_socket.listen(10)
+ 
+    # Add server socket to the list of readable connections
+    CONNECTION_LIST.append(server_socket)
+ 
+    print "Chat server started on port " + str(PORT)
+ 
+    while 1:
+        # Get the list sockets which are ready to be read through select
+        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
+ 
+        for sock in read_sockets:
+            #New connection
+            if sock == server_socket:
+                # Handle the case in which there is a new connection recieved through server_socket
+                sockfd, addr = server_socket.accept()
+                CONNECTION_LIST.append(sockfd)
+                print "Client (%s, %s) connected" % addr
+                 
+                broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
+             
+            #Some incoming message from a client
+            else:
+                # Data recieved from client, process it
+                try:
+                    #In Windows, sometimes when a TCP program closes abruptly,
+                    # a "Connection reset by peer" exception will be thrown
+                    data = sock.recv(RECV_BUFFER)
+                    if data:
+                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)                
+                 
+                except:
+                    broadcast_data(sock, "Client (%s, %s) is offline" % addr)
+                    print "Client (%s, %s) is offline" % addr
+                    sock.close()
+                    CONNECTION_LIST.remove(sock)
+                    continue
+     
+    server_socket.close()
